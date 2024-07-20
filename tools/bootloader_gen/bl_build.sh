a76d75c778f6c3902feb6b3088906ab7d1aa23b3
Daniel Jin <mrpickles123115@gmail.com>
2024-07-20 07:36:32 -0600

    remove unused bl_build.sh

--- tools/bootloader_gen/bl_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python tools/add_magic.py tools/super_secret.txt tools/super_secret.magic
-python tools/add_secrets.py --infile bootloader/bin/bootloader.bin --outfile tools/bl_ready.bin --contents tools/super_secret.magic
-echo finished
