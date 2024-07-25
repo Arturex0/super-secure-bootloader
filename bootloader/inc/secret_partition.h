5ebf8a3198601afd8c1a7f126902e4202c7d0bf3
Daniel Jin <mrpickles123115@gmail.com>
2024-07-24 20:51:29 -0600

    move vault from flash into EEPROM

--- bootloader/inc/secret_partition.h
+++ bootloader/inc/secret_partition.h
@@ -3,7 +3,9 @@
 
 #define SECRETS_BLOCK 55
 #define SECRETS_MAGIC_INDICATOR 0x00002137
+// Ensure these are multiples of blocksize (0x40)
 #define SECRETS_EEPROM_OFFSET 0x400
+#define SECRETS_VAULT_OFFSET 0x3c0
 
 void setup_secrets(void);
 
