f64fa66b53dc8e03e8860593afa45af28e325af3
Daniel Jin <mrpickles123115@gmail.com>
2024-07-26 00:24:53 -0600

    start work on interpreting instructions

--- bootloader/inc/computer.h
+++ bootloader/inc/computer.h
@@ -1,6 +1,7 @@
 #ifndef __BOOTLOADER_COMPUTER_H__
 #define __BOOTLOADER_COMPUTER_H__
 #include <stdint.h>
+#include <stdbool.h>
 
 #define COMP_RA_MASK 0b00000001
 #define COMP_RB_MASK 0b00000010
@@ -8,24 +9,40 @@
 #define COMP_RD_MASK 0b00001000
 #define COMP_RE_MASK 0b00010000
 #define COMP_RF_MASK 0b00100000
-#define COMP_RG_MASK 0b01000000
 
-#define COMP_ADD_CODE
-#define COMP_SUB_CODE
-#define COMP_IMM_CODE
-#define COMP_CMP_CODE
-#define COMP_STM_CODE
-#define COMP_LDM_CODE
-#define COMP_SYS_CODE
-#define COMP_JMP_CODE
-#define COMP_BNE_CODE
-#define COMP_BLT_CODE
+#define COMP_ADD_CODE 0x40
+#define COMP_SUB_CODE 0x41
+#define COMP_IMM_CODE 0x42
+#define COMP_CMP_CODE 0x43
+#define COMP_STM_CODE 0x44
+#define COMP_LDM_CODE 0x45
+#define COMP_SYS_CODE 0x46
+
+#define COMP_JMP_CODE 0x47
+#define COMP_JNE_CODE 0x48
+#define COMP_JLT_CODE 0x49
+
+#define COMP_AND_CODE 0x50
+#define COMP_NOT_CODE 0x51
+#define COMP_ORR_CODE 0x52
+#define COMP_XOR_CODE 0x53
 
 #define COMP_SYS_WRITE
 #define COMP_SYS_READ
 #define COMP_SYS_EXIT
 
-typedef struct _ {
+#define COMP_FLAG_SIGN_SHIFT 0
+#define COMP_FLAG_CARRY_SHIFT 1
+#define COMP_FLAG_ZERO_SHIFT 2
+#define COMP_FLAG_OVERFLOW_SHIFT 3
+
+typedef struct _ins {
+	uint8_t opcode;
+	uint8_t a;
+	uint8_t b;
+} computer_instruction;
+
+typedef struct _comp {
 	uint8_t ip;
 	uint8_t fl;
 	uint8_t ra;
@@ -34,14 +51,32 @@ typedef struct _ {
 	uint8_t rd;
 	uint8_t re;
 	uint8_t rf;
-	// Care must be taken to ensure this buffer is at least 256 bytes
-	uint8_t *instructions;
+	// Care must be taken to ensure this buffer is 256 instructions
+	// If we make the ip refer to instruction rather than offset we can fit 256 instructions
+	// versus 256 // 3 = 85 instructions
+	computer_instruction *instructions;
 	uint8_t memory[256];
 } computer_state;
 
 // Returns exit code after SYS exit
-uint8_t interpret_program(computer_state *);
+uint8_t computer_interpret_program(computer_state *);
+uint8_t computer_read_reg(computer_state* state, uint8_t reg_num);
+void computer_write_reg(computer_state* state, uint8_t reg_num, uint8_t value);
 
-
+void computer_badins(void);
+void computer_add(computer_state *state, uint8_t a, uint8_t b);
+void computer_sub(computer_state *state, uint8_t a, uint8_t b);
+void computer_imm(computer_state *state, uint8_t a, uint8_t b);
+void computer_cmp(computer_state *state, uint8_t a, uint8_t b);
+void computer_stm(computer_state *state, uint8_t a, uint8_t b);
+void computer_ldm(computer_state *state, uint8_t a, uint8_t b);
+bool computer_sys(computer_state *state, uint8_t a, uint8_t b);
+void computer_jmp(computer_state *state, uint8_t a, uint8_t b);
+void computer_jne(computer_state *state, uint8_t a, uint8_t b);
+void computer_jlt(computer_state *state, uint8_t a, uint8_t b);
+void computer_and(computer_state *state, uint8_t a, uint8_t b);
+void computer_not(computer_state *state, uint8_t a, uint8_t b);
+void computer_orr(computer_state *state, uint8_t a, uint8_t b);
+void computer_xor(computer_state *state, uint8_t a, uint8_t b);
 
 #endif
--- bootloader/src/computer.c
+++ bootloader/src/computer.c
@@ -1,11 +1,251 @@
+#include "bootloader.h"
+
+// Hardware Imports
+#include "inc/hw_memmap.h"    // Peripheral Base Addresses
+#include "inc/hw_types.h"     // Boolean type
+#include "inc/tm4c123gh6pm.h" // Peripheral Bit Masks and Registers
+
+// Driver API Imports
+#include "driverlib/sysctl.h"    // System control API (clock/reset)
+#include "driverlib/uart.h"
+
+// Application Imports
+#include "uart/uart.h"
+
+
 #include "computer.h"
+#include <stdbool.h>
 /*
  * 8-bit Harvard architecture, 6 general purpose registers, 
  *
  */
 
+
 uint8_t interpret_program(computer_state* state) {
 	uint8_t opcode;
 	uint8_t a;
 	uint8_t b;
+	computer_instruction ins;
+	bool end = false;
+	bool increment_ip; 
+	while (!end) {
+		increment_ip = true;
+		ins = state->instructions[state->ip];
+		switch (ins.opcode) {
+			case COMP_ADD_CODE:
+				computer_add(state, ins.a, ins.b);
+				break;
+			case COMP_SUB_CODE:
+				computer_sub(state, ins.a, ins.b);
+				break;
+			case COMP_IMM_CODE:
+				computer_imm(state, ins.a, ins.b);
+				break;
+			case COMP_CMP_CODE:
+				computer_cmp(state, ins.a, ins.b);
+				break;
+			case COMP_STM_CODE:
+				computer_stm(state, ins.a, ins.b);
+				break;
+			case COMP_LDM_CODE:
+				computer_ldm(state, ins.a, ins.b);
+				break;
+			case COMP_SYS_CODE:
+				end = computer_sys(state, ins.a, ins.b);
+				break;
+
+			case COMP_JMP_CODE:
+				computer_jmp(state, ins.a, ins.b);
+				increment_ip = false;
+				break;
+			case COMP_JNE_CODE:
+				computer_jne(state, ins.a, ins.b);
+				increment_ip = false;
+				break;
+			case COMP_JLT_CODE:
+				computer_add(state, ins.a, ins.b);
+				increment_ip = false;
+				break;
+
+			case COMP_AND_CODE:
+				computer_and(state, ins.a, ins.b);
+				break;
+			case COMP_NOT_CODE:
+				computer_not(state, ins.a, ins.b);
+				break;
+			case COMP_ORR_CODE:
+				computer_orr(state, ins.a, ins.b);
+				break;
+			case COMP_XOR_CODE:
+				computer_add(state, ins.a, ins.b);
+				break;
+			default:
+				computer_badins();
+
+		}
+		if (increment_ip) {
+			state->ip += 1;
+		}
+	}
+	return 0;
+}
+
+void computer_badins(void) {
+	uart_write_str(UART0, "Bad/unimplemented instruction\n");
+	while(UARTBusy(UART0_BASE)){}
+	SysCtlReset();
+}
+
+// Ra <- Ra + Rb
+void computer_add(computer_state* state, uint8_t a, uint8_t b) {
+	computer_write_reg(state, a, (computer_read_reg(state, a) + computer_read_reg(state, b)));
+}
+
+// Ra <- Ra - Rb
+void computer_sub(computer_state* state, uint8_t a, uint8_t b) {
+	computer_write_reg(state, a, (computer_read_reg(state, a) - computer_read_reg(state, b)));
+}
+
+// Ra <- b
+void computer_imm(computer_state* state, uint8_t a, uint8_t b) {
+	computer_write_reg(state, a, b);
+}
+
+//TODO: idk if this works lol
+void computer_cmp(computer_state* state, uint8_t a, uint8_t b) {
+	uint8_t ra = computer_read_reg(state, a);
+	uint8_t rb = computer_read_reg(state, b);
+
+	// negate rb
+	uint8_t rbn = ((!computer_read_reg(state, b)) + 1);
+
+	uint8_t signa = ((ra >> 7) & 1);
+	uint8_t signb = ((rb >> 7) & 1);
+
+	uint16_t result = (uint16_t) ra + (uint16_t) rbn;
+	uint8_t signr = (result >> 7) & 1;
+	uint8_t carry = (result >> 8) & 1;
+	uint8_t zero = result == 0;
+	uint8_t overflow = (!(signa ^ signb)) & (signr ^ signa);
+
+	state->fl = signr << COMP_FLAG_SIGN_SHIFT | \
+				carry << COMP_FLAG_CARRY_SHIFT | \
+				zero << COMP_FLAG_ZERO_SHIFT | \
+				overflow << COMP_FLAG_OVERFLOW_SHIFT;
+}
+
+
+// *Ma = Rb
+void computer_stm(computer_state* state, uint8_t a, uint8_t b) {
+	state->memory[a] = computer_read_reg(state, b);
+}
+
+// Ra = *Mb
+void computer_ldm(computer_state* state, uint8_t a, uint8_t b) {
+	computer_write_reg(state, a, state->memory[b]);
+}
+
+//TODO: fixme
+bool computer_sys(computer_state* state, uint8_t a, uint8_t b) {
+	computer_badins();
+	return false;
+}
+
+// b is unused, ip = a
+void computer_jmp(computer_state* state, uint8_t a, uint8_t b) {
+	state->ip = a;
+}
+
+// b is unused, ip = a if zero is set
+void computer_jne(computer_state* state, uint8_t a, uint8_t b) {
+	state->ip+=1;
+	if ((state->fl >> COMP_FLAG_ZERO_SHIFT) & 1) {
+		state->ip = a;
+	}
+	
+}
+
+// sf <> of
+void computer_jls(computer_state* state, uint8_t a, uint8_t b) {
+	state->ip+=1;
+	if (((state->fl >> COMP_FLAG_SIGN_SHIFT) & 1) ^ \
+		((state->fl >> COMP_FLAG_OVERFLOW_SHIFT) & 1)) {
+		state->ip = a;
+	}
+}
+
+// Ra <- Ra & Rb
+void computer_and(computer_state* state, uint8_t a, uint8_t b) {
+	computer_write_reg(state, a, (computer_read_reg(state, a) & computer_read_reg(state, b)));
+}
+
+// Ra <- !Rb
+void computer_not(computer_state* state, uint8_t a, uint8_t b) {
+	computer_write_reg(state, a, !computer_read_reg(state, b));
+}
+
+// Ra <- Ra | Rb
+void computer_orr(computer_state* state, uint8_t a, uint8_t b) {
+	computer_write_reg(state, a, (computer_read_reg(state, a) | computer_read_reg(state, b)));
+}
+
+void computer_xor(computer_state* state, uint8_t a, uint8_t b) {
+	computer_write_reg(state, a, (computer_read_reg(state, a) ^ computer_read_reg(state, b)));
+}
+
+uint8_t computer_read_reg(computer_state* state, uint8_t reg_num) {
+	switch (reg_num) {
+		case COMP_RA_MASK:
+			return state->ra;
+			break;
+		case COMP_RB_MASK:
+			return state->rb;
+			break;
+		case COMP_RC_MASK:
+			return state->rc;
+			break;
+		case COMP_RD_MASK:
+			return state->rd;
+			break;
+		case COMP_RE_MASK:
+			return state->re;
+			break;
+		case COMP_RF_MASK:
+			return state->rf;
+			break;
+		default:
+			uart_write_str(UART0, "Invalid register number\n");
+			while(UARTBusy(UART0_BASE)){}
+			SysCtlReset();
+	}
+	return 0;
+
+}
+
+void computer_write_reg(computer_state* state, uint8_t reg_num, uint8_t value) {
+	switch (reg_num) {
+		case COMP_RA_MASK:
+			state->ra = value;
+			break;
+		case COMP_RB_MASK:
+			state->rb = value;
+			break;
+		case COMP_RC_MASK:
+			state->rc = value;
+			break;
+		case COMP_RD_MASK:
+			state->rd = value;
+			break;
+		case COMP_RE_MASK:
+			state->re = value;
+			break;
+		case COMP_RF_MASK:
+			state->rf = value;
+			break;
+		default:
+			uart_write_str(UART0, "Invalid register number\n");
+			while(UARTBusy(UART0_BASE)){}
+			SysCtlReset();
+	}
+	return;
 }
